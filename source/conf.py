22acaf5f4732099e2c99e32830a9835028d23a26
czk <chenzikunczk@gmail.com>
2017-07-02 20:40:02 +0800

    修改配置文件

--- source/conf.py
+++ source/conf.py
@@ -41,6 +41,11 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 source_suffix = ['.rst', '.md']
 
 
